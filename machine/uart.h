95be65779fab9b5c0179ddb85e0de1f9de7cba8e
Sagar Karandikar <sagark@eecs.berkeley.edu>
2018-09-23 16:42:58 +0000

    fix UART_REG_DIV number to match sifive-blocks

--- machine/uart.h
+++ machine/uart.h
@@ -9,7 +9,7 @@ extern volatile uint32_t* uart;
 #define UART_REG_RXFIFO		1
 #define UART_REG_TXCTRL		2
 #define UART_REG_RXCTRL		3
-#define UART_REG_DIV		4
+#define UART_REG_DIV		6
 
 #define UART_TXEN		 0x1
 #define UART_RXEN		 0x1

83d416e5e5906e0f68683eff8c37492a20ddc765
David Kohlbrenner <dkohlbre@cs.ucsd.edu>
2018-11-01 14:47:04 -0700

    Switched some of the SBI calls over to a struct-passing system. Only CREATE and RUN have changed for now.

--- machine/mtrap.c
+++ machine/mtrap.c
@@ -128,7 +128,7 @@ void mcall_trap(uintptr_t* regs, uintptr_t mcause, uintptr_t mepc)
 {
   write_csr(mepc, mepc + 4);
 
-  uintptr_t n = regs[17], arg0 = regs[10], arg1 = regs[11], arg2 = regs[12], arg3 = regs[13], retval, ipi_type;
+  uintptr_t n = regs[17], arg0 = regs[10], arg1 = regs[11], arg2 = regs[12], arg3 = regs[13], retval, ipi_type, entry_point;
 
   switch (n)
   {
@@ -166,17 +166,18 @@ send_ipi:
       break;
 #ifdef SM_ENABLED
     case SBI_SM_CREATE_ENCLAVE:
-      retval = mcall_sm_create_enclave(arg0, arg1, arg2);
+      retval = mcall_sm_create_enclave(arg0);
       break;
     case SBI_SM_DESTROY_ENCLAVE:
       retval = mcall_sm_destroy_enclave(arg0);
       break;
     case SBI_SM_RUN_ENCLAVE:
-      retval = mcall_sm_run_enclave(regs, arg0, arg1, arg2);
+      retval = mcall_sm_run_enclave(regs, arg0, &entry_point);
       if(!retval)
       {
-      /* the entry point is passed to the runtime through $a0 */
-        retval = arg1;
+      /* TODO: have a cleaner way to do this
+	 the entry point is passed to the runtime through $a0 */
+        retval = entry_point;
       }
       break;
     case SBI_SM_EXIT_ENCLAVE:

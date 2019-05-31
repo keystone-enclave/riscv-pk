6192483a2b4d27bfc3d60773d72b746443649cff
Dayeol Lee <dayeol@berkeley.edu>
2019-05-31 16:59:49 +0000

    Adding tests (cont'd)

--- sm/tests/pmp_test.c
+++ sm/tests/pmp_test.c
@@ -11,6 +11,7 @@
 
 #include "../pmp.c"
 
+
 static void test_search_rightmost_unset()
 {
   // static int search_rightmost_unset(uint32 bitmap, int max, uint32_t mask)
@@ -76,6 +77,78 @@ static void test_get_conseq_free_reg_idx()
   reg_bitmap = 0x0;
 }
 
+static void test_pmp_region_init_and_free()
+{
+  assert_int_equal(region_def_bitmap, 0x0);
+  region_id rid;
+  // region from 0x8000 - 0xc000
+  pmp_region_init_atomic(0x8000, 0x4000, PMP_PRI_TOP, &rid, false);
+
+  assert_int_equal(region_def_bitmap, 0x1);
+  assert_int_equal(reg_bitmap, 0x1);
+  assert_int_equal(rid, 0);
+  assert_int_equal(regions[0].size, 0x4000);
+  assert_int_equal(regions[0].addrmode, PMP_NAPOT);
+  assert_int_equal(regions[0].addr, 0x8000);
+  assert_int_equal(regions[0].allow_overlap, false);
+  assert_int_equal(regions[0].reg_idx, 0);
+
+  pmp_region_free_atomic(0);
+
+  assert_int_equal(region_def_bitmap, 0x0);
+  assert_int_equal(reg_bitmap, 0x0);
+  struct pmp_region zero = {0};
+  assert_memory_equal(&regions[0], &zero, sizeof(struct pmp_region));
+}
+
+static void test_region_helpers()
+{
+  int rid = 4;
+  region_init(rid,
+              0xdeadbeef,
+              0xfeed,
+              PMP_TOR,
+              true,
+              5);
+
+  assert_int_equal(region_register_idx(rid), 6);
+  assert_true(region_allows_overlap(rid));
+  assert_int_equal(region_get_addr(rid), 0xdeadbeef);
+  assert_int_equal(region_get_size(rid), 0xfeed);
+  assert_false(region_is_napot(rid));
+  assert_true(region_is_tor(rid));
+  assert_true(region_needs_two_entries(rid));
+  assert_false(region_is_napot_all(rid));
+  assert_int_equal(region_pmpaddr_val(rid), 0x37ABAF77);
+
+  region_clear_all(rid);
+
+  struct pmp_region zero = {0};
+  assert_memory_equal(&regions[rid], &zero, sizeof(struct pmp_region));
+
+  rid = 7;
+  region_init(rid,
+              0x8000,
+              0x4000,
+              PMP_NAPOT,
+              false,
+              0);
+
+  assert_int_equal(region_register_idx(rid), 0);
+  assert_false(region_allows_overlap(rid));
+  assert_int_equal(region_get_addr(rid), 0x8000);
+  assert_int_equal(region_get_size(rid), 0x4000);
+  assert_true(region_is_napot(rid));
+  assert_false(region_is_tor(rid));
+  assert_false(region_needs_two_entries(rid));
+  assert_false(region_is_napot_all(rid));
+  assert_int_equal(region_pmpaddr_val(rid), 0x27ff);
+
+  region_clear_all(rid);
+
+  assert_memory_equal(&regions[rid], &zero, sizeof(struct pmp_region));
+}
+
 int main()
 {
   const struct CMUnitTest tests[] = {
@@ -83,6 +156,8 @@ int main()
     cmocka_unit_test(test_get_free_region_idx),
     cmocka_unit_test(test_get_free_reg_idx),
     cmocka_unit_test(test_get_conseq_free_reg_idx),
+    cmocka_unit_test(test_pmp_region_init_and_free),
+    cmocka_unit_test(test_region_helpers),
   };
 
   return cmocka_run_group_tests(tests, NULL, NULL);

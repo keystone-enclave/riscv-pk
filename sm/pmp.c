92c357a70336868dd40dba0bfbe56b8492518df0
Dayeol Lee <dayeol@berkeley.edu>
2019-06-25 18:41:50 +0000

    rv32

--- sm/pmp.c
+++ sm/pmp.c
@@ -171,7 +171,7 @@ static int detect_region_overlap(uintptr_t addr, uintptr_t size)
 
   // Safety check the addr+size
   uintptr_t input_end;
-  if( __checked_uaddl(addr, size, &input_end)){
+  if( CHECKED_ADD(addr, size, &input_end)){
     return 1;
   }
 

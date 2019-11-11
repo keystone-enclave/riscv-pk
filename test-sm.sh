ab9307970345f96466865195181ad758754db87e
Gui Andrade <gh@archshift.com>
2019-11-10 19:04:27 -0800

    Enable building with optimizations for Rust SM, delete soft float
    Fix specifying RUST_TARGET for Makefile (actually honor the
    passed in target)

--- test-sm.sh
+++ test-sm.sh
@@ -7,9 +7,9 @@ cd test-build
 if [ ! -f Makefile ]; then
 	../configure --host=riscv64-unknown-linux-gnu --enable-sm
 fi
-make -j12 RUST_TARGET=riscv64gc-unknown-linux-gnuhf.json
+make -j12 RUST_TARGET=../riscv64gc-unknown-none-elfhf.json
 
-export RUSTFLAGS="-Z pre-link-arg=-L$(pwd) -Z pre-link-arg=-lsm_Clib -C link-arg=-Wl,--start-group -C link-arg=-lmachine -C link-arg=-lsoftfloat -C link-arg=-lutil -C link-arg=-lsm_Clib -C link-arg=-Wl,--end-group"
+export RUSTFLAGS="-Z pre-link-arg=-L$(pwd) -Z pre-link-arg=-lsm_Clib -C link-arg=-Wl,--start-group -C link-arg=-lmachine -C link-arg=-lutil -C link-arg=-lsm_Clib -C link-arg=-Wl,--end-group"
 export CFLAGS='-DTARGET_PLATFORM_HEADER="platform/default/default.h"'
 cargo +nightly xtest --target-dir sm_rs/ --target ../riscv64gc-unknown-linux-gnuhf.json --manifest-path=../sm/Cargo.toml
 

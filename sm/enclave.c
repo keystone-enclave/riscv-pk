594a5d28036a28ea3aa5fd6714ad2d068597a725
Alex Thomas <alexthomas@berkeley.edu>
2019-11-18 06:34:39 +0000

    More clean up

--- sm/enclave.c
+++ sm/enclave.c
@@ -41,7 +41,6 @@ static inline enclave_ret_code context_switch_to_enclave(uintptr_t* regs,
                                                 int load_parameters){
 
 
-//printm("IN ENCLAVE: enclave satp: %p, curr satp: %p\n", enclaves[eid].encl_satp, read_csr(satp));
   /* save host context */
   swap_prev_state(&enclaves[eid].threads[0], regs);
   swap_prev_mepc(&enclaves[eid].threads[0], read_csr(mepc));
@@ -87,17 +86,6 @@ static inline enclave_ret_code context_switch_to_enclave(uintptr_t* regs,
   uintptr_t interrupts = MIP_SSIP | MIP_SEIP;
   write_csr(mideleg, interrupts);
 
-
-  // Clear pending interrupts
-//  clear_csr(mip, MIP_MTIP);
-//  clear_csr(mip, MIP_STIP);
-//  clear_csr(mip, MIP_SSIP);
-//  clear_csr(mip, MIP_SEIP);
-
-//  printm("%b", read_csr(mip));
-  //printm("%b", read_csr(mie));
-
-
   // set PMP
   osm_pmp_set(PMP_NO_PERM);
   int memid;
@@ -109,7 +97,6 @@ static inline enclave_ret_code context_switch_to_enclave(uintptr_t* regs,
 
   // Setup any platform specific defenses
   platform_switch_to_enclave(&(enclaves[eid]));
- // printm("IN ENCLAVE: enclave satp: %p, curr satp: %p\n", enclaves[eid].encl_satp, read_csr(satp));
   cpu_enter_enclave_context(eid);
   return ENCLAVE_SUCCESS;
 }
@@ -126,9 +113,6 @@ static inline void context_switch_to_host(uintptr_t* encl_regs,
   }
   osm_pmp_set(PMP_ALL_PERM);
 
-  //printm("IN HOST: eid: %d, enclave satp: %p, host: satp: %p\n", eid, encl.encl_satp, read_csr(satp));
-  //printm("encl_regs in host: %p\n", encl_regs); 
-
   /* restore host context */
   swap_prev_state(&enclaves[eid].threads[0], encl_regs);
   swap_prev_mepc(&enclaves[eid].threads[0], read_csr(mepc));
@@ -140,17 +124,12 @@ static inline void context_switch_to_host(uintptr_t* encl_regs,
   write_csr(mideleg, interrupts);
 
   // enable timer interrupt
- // set_csr(mie, MIP_MTIP);
   clear_csr(mie, MIP_MTIP);
   clear_csr(mip, MIP_STIP); 
   clear_csr(mip, MIP_MTIP); 
 
   // Reconfigure platform specific defenses
   platform_switch_from_enclave(&(enclaves[eid]));
-
-  //printm("after encl_regs in host: %p\n", encl_regs);
-
-  //printm("IN HOST: enclave satp: %p, host: satp: %p\n", encl.encl_satp, read_csr(satp)); 
   cpu_exit_enclave_context();
   return;
 }

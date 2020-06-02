32187d85d25157c2ddc38e31f30f88b20e28d21e
Alex Thomas <alexthomas@berkeley.edu>
2020-06-02 04:57:01 +0000

    Add rv32 support for mprv

--- sm/mprv.h
+++ sm/mprv.h
@@ -3,36 +3,44 @@
 #include <stdbool.h>
 
 int copy1_from_sm(uintptr_t dst, const uint8_t *src);
-int copy8_from_sm(uintptr_t dst, const uint64_t *src);
-int copy64_from_sm(uintptr_t dst, const uint64_t *src);
+int copy_word_from_sm(uintptr_t dst, const uint64_t *src);
+int copy_block_from_sm(uintptr_t dst, const uint64_t *src);
 
 int copy1_to_sm(uint8_t *dst, uintptr_t src);
-int copy8_to_sm(uint64_t *dst, uintptr_t src);
-int copy64_to_sm(uint64_t *dst, uintptr_t src);
+int copy_word_to_sm(uint64_t *dst, uintptr_t src);
+int copy_block_to_sm(uint64_t *dst, uintptr_t src);
+
+#if __riscv_xlen == 64
+#define MPRV_BLOCK 64
+#define MPRV_WORD 8
+#elif __riscv_xlen == 32
+#define MPRV_BLOCK 32
+#define MPRV_WORD 4
+#endif
 
 int copy_from_sm(uintptr_t dst, void *src_buf, size_t len)
 {
     uintptr_t src = (uintptr_t)src_buf;
 
-    if (src % 4 == 0 && dst % 4 == 0) {
-        while (len >= 32) {
-            int res = copy64_from_sm(dst, (uint64_t *)src);
+    if (src % MPRV_WORD == 0 && dst % MPRV_WORD == 0) {
+        while (len >= MPRV_BLOCK) {
+            int res = copy_block_from_sm(dst, (uint64_t *)src);
             if (res)
                 return res;
             
-            src += 32;
-            dst += 32;
-            len -= 32;
+            src += MPRV_BLOCK;
+            dst += MPRV_BLOCK;
+            len -= MPRV_BLOCK;
         }
 
-        while (len >= 4) {
-            int res = copy8_from_sm(dst, (uint64_t *)src);
+        while (len >= MPRV_WORD) {
+            int res = copy_word_from_sm(dst, (uint64_t *)src);
             if (res)
                 return res;
 
-            src += 4;
-            dst += 4;
-            len -= 4;
+            src += MPRV_WORD;
+            dst += MPRV_WORD;
+            len -= MPRV_WORD;
         }
     }
 
@@ -53,25 +61,25 @@ int copy_to_sm(void *dst_buf, uintptr_t src, size_t len)
 {
     uintptr_t dst = (uintptr_t)dst_buf;
 
-    if (src % 4 == 0 && dst % 4 == 0) {
-        while (len >= 32) {
-            int res = copy64_to_sm((uint64_t *)dst, src);
+    if (src % MPRV_WORD == 0 && dst % MPRV_WORD == 0) {
+        while (len >= MPRV_BLOCK) {
+            int res = copy_block_to_sm((uint64_t *)dst, src);
             if (res)
                 return res;
             
-            src += 32;
-            dst += 32;
-            len -= 32;
+            src += MPRV_BLOCK;
+            dst += MPRV_BLOCK;
+            len -= MPRV_BLOCK;
         }
 
-        while (len >= 4) {
-            int res = copy8_to_sm((uint64_t *)dst, src);
+        while (len >= MPRV_WORD) {
+            int res = copy_word_to_sm((uint64_t *)dst, src);
             if (res)
                 return res;
 
-            src += 4;
-            dst += 4;
-            len -= 4;
+            src += MPRV_WORD;
+            dst += MPRV_WORD;
+            len -= MPRV_WORD;
         }
     }
 

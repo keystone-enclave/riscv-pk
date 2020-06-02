57f3fb906684c364e8bc23a298f9a5fc1ca53e7e
Alex Thomas <alexthomas@berkeley.edu>
2020-06-02 05:27:06 +0000

    Make MPRV offsets indep of arch

--- sm/mprv.h
+++ sm/mprv.h
@@ -13,9 +13,25 @@ int copy_block_to_sm(uint64_t *dst, uintptr_t src);
 #if __riscv_xlen == 64
 #define MPRV_BLOCK 64
 #define MPRV_WORD 8
+#define MPRV_OFF_0 (0x0)
+#define MPRV_OFF_1 0x8
+#define MPRV_OFF_2 0x10
+#define MPRV_OFF_3 0x18
+#define MPRV_OFF_4 0x20
+#define MPRV_OFF_5 0x28
+#define MPRV_OFF_6 0x30
+#define MPRV_OFF_7 0x38
 #elif __riscv_xlen == 32
 #define MPRV_BLOCK 32
 #define MPRV_WORD 4
+#define MPRV_OFF_0 0x0
+#define MPRV_OFF_1 0x4
+#define MPRV_OFF_2 0x8
+#define MPRV_OFF_3 0xc
+#define MPRV_OFF_4 0x10
+#define MPRV_OFF_5 0x14
+#define MPRV_OFF_6 0x18
+#define MPRV_OFF_7 0x1c
 #endif
 
 int copy_from_sm(uintptr_t dst, void *src_buf, size_t len)

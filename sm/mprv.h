95fd73a8521b528ef689191db4c99899e71ba132
Alex Thomas <alexthomas@berkeley.edu>
2020-06-05 02:33:03 +0000

    Fix type conventions

--- sm/mprv.h
+++ sm/mprv.h
@@ -2,15 +2,15 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-typedef struct { size_t words[8]; } mprv_block_t;
+typedef struct { size_t words[8]; } mprv_block;
 
 int copy1_from_sm(uintptr_t dst, const uint8_t *src);
 int copy_word_from_sm(uintptr_t dst, const uintptr_t *src);
-int copy_block_from_sm(uintptr_t dst, const mprv_block_t *src);
+int copy_block_from_sm(uintptr_t dst, const mprv_block *src);
 
 int copy1_to_sm(uint8_t *dst, uintptr_t src);
 int copy_word_to_sm(uintptr_t *dst, uintptr_t src);
-int copy_block_to_sm(mprv_block_t *dst, uintptr_t src);
+int copy_block_to_sm(mprv_block *dst, uintptr_t src);
 
 #if __riscv_xlen == 64
 # define STORE    sd
@@ -31,7 +31,7 @@ int copy_from_sm(uintptr_t dst, void *src_buf, size_t len)
 
     if (src % REGBYTES  == 0 && dst % REGBYTES == 0) {
         while (len >= MPRV_BLOCK) {
-            int res = copy_block_from_sm(dst, (mprv_block_t *)src);
+            int res = copy_block_from_sm(dst, (mprv_block *)src);
             if (res)
                 return res;
             
@@ -70,7 +70,7 @@ int copy_to_sm(void *dst_buf, uintptr_t src, size_t len)
 
     if (src % REGBYTES == 0 && dst % REGBYTES == 0) {
         while (len >= MPRV_BLOCK) {
-            int res = copy_block_to_sm((mprv_block_t *)dst, src);
+            int res = copy_block_to_sm((mprv_block *)dst, src);
             if (res)
                 return res;
             
